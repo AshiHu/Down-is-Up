11a7601d25a6f70bd0033829e6020570a8ae9645
AshiHu <alex.bely515@gmail.com>
2026-05-21 15:31:36 +0200

    porte dessolve

--- Assets/Script/Niveau1 code/Door.cs
+++ Assets/Script/Niveau1 code/Door.cs
@@ -1,47 +1,50 @@
+﻿using UnityEngine;
 using System.Collections;
-using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    [Header("Dessolve")]
-    public Material doorMaterial;
-    public float dessolveDuration = 0f;
+    [Header("Dissolve Settings")]
+    private Renderer Rend;
+    private Material doorMaterial;
 
-    //gestion de l'animation 
-    [Header("Animation")]
-    public Vector3 openOffset = new Vector3(0f, 3f, 0f);
-    public float duration = 1f;
-    public AnimationCurve curve = AnimationCurve.EaseInOut(0, 0, 1, 1);
+    [Tooltip("Durée de l'effet en secondes")]
+    public float dissolveDuration = 1f;
 
-    private bool _opened = false;
+    private bool _triggered = false;
 
-    // ouverture de la porte 
+    private void Start()
+    {
+        Rend = GetComponent<Renderer>();
+        if (Rend != null)
+        {
+            doorMaterial = Rend.material;
+            Debug.Log("Material : " + doorMaterial.name);
+        }
+        else
+        {
+            Debug.LogError("Il manque un MeshRenderer ou SpriteRenderer sur ce GameObject !");
+        }
+    }
     public void Open()
     {
-        if (_opened) return;
-        _opened = true;
-        StartCoroutine(Slide(transform.position + openOffset));
+        if (_triggered || doorMaterial == null) return;
+        _triggered = true;
+        StartCoroutine(Dissolve());
     }
 
-    // animation de la porte du point start a la position voulue
-    private IEnumerator Slide(Vector3 target)
+    private IEnumerator Dissolve()
     {
-        doorMaterial.SetFloat("_DissolveAmount", 0f);
-
-        Vector3 start = transform.position;
         float elapsed = 0f;
+        doorMaterial.SetFloat("_Dessolve", 0f);
 
-        while (elapsed < duration)
+        while (elapsed < dissolveDuration)
         {
             elapsed += Time.deltaTime;
-            float t = curve.Evaluate(elapsed / duration);
-
-            // utilisation d'un lerp pour un deplacement image par image pour de la fluidite
-            transform.position = Vector3.Lerp(start, target, t);
+            float progress = Mathf.Clamp01(elapsed / dissolveDuration);
+            doorMaterial.SetFloat("_Dessolve", progress);
             yield return null;
         }
 
-        transform.position = target;
-        doorMaterial.SetFloat("_DissolveAmount", 1f);
+        doorMaterial.SetFloat("_Dessolve", 1f);
     }
 }
