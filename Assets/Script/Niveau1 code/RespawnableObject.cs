84ffa63a1cf45dea891a696f2ec19d4ab45b11c3
AshiHu <alex.bely515@gmail.com>
2026-06-04 01:03:54 +0200

    changement

--- Assets/Script/Niveau1 code/RespawnableObject.cs
+++ Assets/Script/Niveau1 code/RespawnableObject.cs
@@ -3,19 +3,34 @@ using UnityEngine;
 public class RespawnableObject : MonoBehaviour
 {
     public Transform respawnPoint;
+    public float respawnDelay = 10f;
+
     private Rigidbody _rb;
+    private Vector3 _initialPosition;
+    private Quaternion _initialRotation;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
+        _initialPosition = respawnPoint != null ? respawnPoint.position : transform.position;
+        _initialRotation = respawnPoint != null ? respawnPoint.rotation : transform.rotation;
     }
 
-    // Fonction pour reinitialiser la position et les vitesses de l'objet
+    private void OnEnable()
+    {
+        InvokeRepeating(nameof(Respawn), respawnDelay, respawnDelay);
+    }
+
+    private void OnDisable()
+    {
+        CancelInvoke(nameof(Respawn));
+    }
+
     public void Respawn()
     {
         _rb.linearVelocity = Vector3.zero;
         _rb.angularVelocity = Vector3.zero;
-        transform.position = respawnPoint.position;
-        transform.rotation = respawnPoint.rotation;
+        transform.position = respawnPoint != null ? respawnPoint.position : _initialPosition;
+        transform.rotation = respawnPoint != null ? respawnPoint.rotation : _initialRotation;
     }
 }
